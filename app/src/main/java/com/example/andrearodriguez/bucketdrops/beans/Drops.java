1c7f37674433fb365cfc8dc811cf497f18c839a9
liyi85 <liyi85@gmail.com>
2016-06-21 15:03:23 -0500

    implementacion del realm, descarga de archivo realm, creacion de 3 aventos

---
+++ app/src/main/java/com/example/andrearodriguez/bucketdrops/beans/Drops.java
@@ -0,0 +1,59 @@
+package com.example.andrearodriguez.bucketdrops.beans;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by andrearodriguez on 6/17/16.
+ */
+public class Drops extends RealmObject {
+
+    public Drops(String what, long added, long when, boolean completed) {
+        this.what = what;
+        this.added = added;
+        this.when = when;
+        this.completed = completed;
+    }
+
+    private String what;
+    @PrimaryKey
+    private long added;
+    private long when;
+    private boolean completed;
+
+    public Drops() {
+    }
+
+    public String getWhat() {
+        return what;
+    }
+
+    public void setWhat(String what) {
+        this.what = what;
+    }
+
+    public long getAdded() {
+        return added;
+    }
+
+    public void setAdded(long added) {
+        this.added = added;
+    }
+
+    public long getWhen() {
+        return when;
+    }
+
+    public void setWhen(long when) {
+        this.when = when;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
+
